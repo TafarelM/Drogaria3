627fa074eda74a035d5ce6f5f2302a2054047466
Tafarel Mello <tafarel.mello@gmail.com>
2017-02-24 04:23:29 -0300

    omnifaces

--- src/main/java/com/tafarelmello/drogaria/controller/EstadoController.java
+++ src/main/java/com/tafarelmello/drogaria/controller/EstadoController.java
@@ -2,10 +2,12 @@ package com.tafarelmello.drogaria.controller;
 
 import javax.faces.bean.ManagedBean;
 
+import org.omnifaces.util.Messages;
+
 @ManagedBean
 public class EstadoController {
 	
 	public void salvar(){
-		
+		Messages.addGlobalInfo("TEST");
 	}
 }
