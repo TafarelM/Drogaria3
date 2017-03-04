8abd71c4d291654ec18c85fe88d9b571a2d49269
Tafarel Mello <tafarel.mello@gmail.com>
2017-03-04 01:18:14 -0300

    rest fabricante

--- src/main/java/com/tafarelmello/drogaria/controller/FabricanteController.java
+++ src/main/java/com/tafarelmello/drogaria/controller/FabricanteController.java
@@ -1,15 +1,21 @@
 package com.tafarelmello.drogaria.controller;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
 
 import org.omnifaces.util.Messages;
 
+import com.google.gson.Gson;
 import com.tafarelmello.drogaria.dao.FabricanteDAO;
 import com.tafarelmello.drogaria.domain.Fabricante;
 
@@ -29,13 +35,22 @@ public class FabricanteController implements Serializable {
 	public void novo() {
 
 		fabricante = new Fabricante();
-
 	}
 
 	public void salvar() {
 		try {
-			FabricanteDAO fabricanteDAO = new FabricanteDAO();
-			fabricanteDAO.merge(fabricante);
+			/*
+			 * FabricanteDAO fabricanteDAO = new FabricanteDAO();
+			 * fabricanteDAO.merge(fabricante);
+			 */
+
+			Client client = ClientBuilder.newClient();
+			WebTarget caminho = client.target("http://localhost:8080/Drogaria/rest/fabricante");
+
+			Gson gson = new Gson();
+			String json = gson.toJson(fabricante);
+
+			caminho.request().post(Entity.json(json));
 
 			novo();
 			listar();
@@ -49,8 +64,19 @@ public class FabricanteController implements Serializable {
 
 	public void listar() {
 		try {
-			FabricanteDAO fabricanteDAO = new FabricanteDAO();
-			fabricantes = fabricanteDAO.listar();
+			/*
+			 * FabricanteDAO fabricanteDAO = new FabricanteDAO(); fabricantes =
+			 * fabricanteDAO.listar();
+			 */
+
+			Client client = ClientBuilder.newClient();
+			WebTarget caminho = client.target("http://localhost:8080/Drogaria/rest/fabricante");
+			String json = caminho.request().get(String.class);
+
+			Gson gson = new Gson();
+			Fabricante[] vetor = gson.fromJson(json, Fabricante[].class);
+
+			fabricantes = Arrays.asList(vetor);
 		} catch (RuntimeException erro) {
 			Messages.addGlobalError("Erro ao tentar listar o Fabricante.");
 			erro.printStackTrace();
@@ -60,12 +86,12 @@ public class FabricanteController implements Serializable {
 	public void excluir(ActionEvent evento) {
 		try {
 			fabricante = (Fabricante) evento.getComponent().getAttributes().get("fabricanteSelecionado");
-			
+
 			FabricanteDAO fabricanteDAO = new FabricanteDAO();
 			fabricanteDAO.excluir(fabricante);
-			
+
 			listar();
-			
+
 			Messages.addFlashGlobalInfo("excluido com sucesso.");
 		} catch (RuntimeException erro) {
 			Messages.addGlobalError("Erro ao tentar excluir o Fabricante.");
@@ -77,7 +103,7 @@ public class FabricanteController implements Serializable {
 		try {
 			fabricante = (Fabricante) evento.getComponent().getAttributes().get("fabricanteSelecionado");
 
-			listar();
+			// listar();
 		} catch (RuntimeException erro) {
 			Messages.addGlobalError("Erro ao tentar editar o Fabricante.");
 			erro.printStackTrace();
--- src/main/java/com/tafarelmello/drogaria/service/FabricanteService.java
+++ src/main/java/com/tafarelmello/drogaria/service/FabricanteService.java
@@ -2,8 +2,10 @@ package com.tafarelmello.drogaria.service;
 
 import java.util.List;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
@@ -13,37 +15,65 @@ import com.tafarelmello.drogaria.domain.Fabricante;
 
 @Path("fabricante")
 public class FabricanteService {
-	
+
 	@GET
-	public String listar(){
+	public String listar() {
 		FabricanteDAO fabricanteDAO = new FabricanteDAO();
 		List<Fabricante> fabricantes = fabricanteDAO.listar("descricao");
-		
+
 		Gson gson = new Gson();
 		String json = gson.toJson(fabricantes);
-		
+
 		return json;
 	}
-	
+
 	@GET
-	@Path("{codigo}")	
-	public String buscar(@PathParam("codigo")Long codigo){
+	@Path("{codigo}")
+	public String buscar(@PathParam("codigo") Long codigo) {
 		FabricanteDAO fabricanteDAO = new FabricanteDAO();
 		Fabricante fabricante = fabricanteDAO.buscar(codigo);
-		
+
 		Gson gson = new Gson();
 		String json = gson.toJson(fabricante);
-		
-		return  json;
+
+		return json;
 	}
-	
+
 	@POST
-	public String salvar(String json){
+	public String salvar(String json) {
 		Gson gson = new Gson();
 		Fabricante fabricante = gson.fromJson(json, Fabricante.class);
-		
+
 		FabricanteDAO fabricanteDAO = new FabricanteDAO();
 		fabricanteDAO.salvar(fabricante);
+
+		String jsonSaida = gson.toJson(fabricante);
+
+		return jsonSaida;
+	}
+
+	@PUT
+	public String editar(String json) {
+		Gson gson = new Gson();
+		Fabricante fabricante = gson.fromJson(json, Fabricante.class);
+
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		fabricanteDAO.editar(fabricante);
+
+		String jsonSaida = gson.toJson(fabricante);
+
+		return jsonSaida;
+	}
+
+	@DELETE
+	public String exlcuir(String json){
+		Gson gson = new Gson();
+		Fabricante fabricante =  gson.fromJson(json, Fabricante.class);
+		
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		fabricante = fabricanteDAO.buscar(fabricante.getCodigo());
+		
+		fabricanteDAO.excluir(fabricante);
 		
 		String jsonSaida = gson.toJson(fabricante);
 		
