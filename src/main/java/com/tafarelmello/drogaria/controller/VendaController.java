6a5cab79bf810c30d1f1c9afdf28be8326bb0175
Tafarel Mello <tafarel.mello@gmail.com>
2017-03-08 23:08:02 -0300

    construindo tela de vendas

--- src/main/java/com/tafarelmello/drogaria/controller/VendaController.java
+++ src/main/java/com/tafarelmello/drogaria/controller/VendaController.java
@@ -1,15 +1,18 @@
 package com.tafarelmello.drogaria.controller;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
 
 import org.omnifaces.util.Messages;
 
 import com.tafarelmello.drogaria.dao.ProdutoDAO;
+import com.tafarelmello.drogaria.domain.ItemVenda;
 import com.tafarelmello.drogaria.domain.Produto;
 
 @SuppressWarnings("serial")
@@ -18,6 +21,7 @@ import com.tafarelmello.drogaria.domain.Produto;
 public class VendaController implements Serializable {
 
 	private List<Produto> produtos;
+	private List<ItemVenda> itensVenda;
 
 	@PostConstruct
 	public void iniciar() {
@@ -28,10 +32,35 @@ public class VendaController implements Serializable {
 		try {
 			ProdutoDAO produtoDAO = new ProdutoDAO();
 			produtos = produtoDAO.listar("descricao");
+			
+			itensVenda = new ArrayList<>();
 		} catch (RuntimeException erro) {
 			Messages.addGlobalError("Ocorreu um erro ao tentar carregar a tela de vendas");
 			erro.printStackTrace();
 		}
+	}
+	
+	public void adicionar(ActionEvent evento){
+		Produto produto = (Produto) evento.getComponent().getAttributes().get("produtoSelecionado");
+		
+		int achou = -1;
+		for(int posicao = 0; posicao < itensVenda.size(); posicao++){
+			if(itensVenda.get(posicao).getProduto().equals(produto)){
+				achou = posicao;
+			}
+		}
+		
+		if(achou < 0){
+			ItemVenda itemVenda = new ItemVenda();
+			itemVenda.setValorParcial(produto.getPreco());
+			itemVenda.setProduto(produto);
+			itemVenda.setQuantidade(new Short("1"));
+			
+			itensVenda.add(itemVenda);
+		}else{
+			ItemVenda itemVenda = itensVenda.get(achou);
+		}
+		
 	}
 
 	public List<Produto> getProdutos() {
@@ -40,6 +69,14 @@ public class VendaController implements Serializable {
 
 	public void setProdutos(List<Produto> produtos) {
 		this.produtos = produtos;
+	}
+
+	public List<ItemVenda> getItensVenda() {
+		return itensVenda;
+	}
+
+	public void setItensVenda(List<ItemVenda> itensVenda) {
+		this.itensVenda = itensVenda;
 	}
 
 }
